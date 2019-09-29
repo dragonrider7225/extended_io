0efc25cff8f01be462a897e230c0bc0065ebecf8
Kevin Moonen <dragonrider7225@gmail.com>
2019-09-29 18:14:52 -0500

    Added support for network-endian reads/writes and signed integer reads/writes

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@ use std::{
  * Read a "big-endian" u8 from the specified bit source. Since big-endian and
  * little-endian refer to byte order, not bit order, there is no difference
  * between the big-endian and little-endian encodings of a single byte.
- * Nevertheless, both `read_u8` and `read_u8_le` are provided for the sake of
- * uniformity.
+ * Nevertheless, `read_u8`, `read_u8_le`, and `read_u8_ne` are provided for the
+ * sake of uniformity.
  */
 pub fn read_u8(src: &mut dyn Read) -> io::Result<u8> {
     let mut buf = [0; 1];
@@ -19,16 +19,68 @@ pub fn read_u8(src: &mut dyn Read) -> io::Result<u8> {
 }
 
 /**
- * Read a "big-endian" u8 from the specified bit source. Since big-endian and
- * little-endian refer to byte order, not bit order, there is no difference
+ * Read a "little-endian" u8 from the specified bit source. Since big-endian
+ * and little-endian refer to byte order, not bit order, there is no difference
  * between the big-endian and little-endian encodings of a single byte.
- * Nevertheless, both `read_u8` and `read_u8_le` are provided for the sake of
- * uniformity.
+ * Nevertheless, `read_u8`, `read_u8_le`, and `read_u8_ne` are provided for the
+ * sake of uniformity.
  */
 pub fn read_u8_le(src: &mut dyn Read) -> io::Result<u8> {
     let mut buf = [0; 1];
     src.read_exact(&mut buf)?;
     Ok(u8::from_le_bytes(buf))
+}
+
+/**
+ * Read a "network-endian" u8 from the specified bit source. Since big-endian
+ * and little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `read_u8`, `read_u8_le`, and `read_u8_ne` are provided for the
+ * sake of uniformity.
+ */
+pub fn read_u8_ne(src: &mut dyn Read) -> io::Result<u8> {
+    let mut buf = [0; 1];
+    src.read_exact(&mut buf)?;
+    Ok(u8::from_ne_bytes(buf))
+}
+
+/**
+ * Read a "big-endian" i8 from the specified bit source. Since big-endian and
+ * little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `read_i8`, `read_i8_le`, and `read_i8_ne` are provided for the
+ * sake of uniformity.
+ */
+pub fn read_i8(src: &mut dyn Read) -> io::Result<i8> {
+    let mut buf = [0; 1];
+    src.read_exact(&mut buf)?;
+    Ok(i8::from_be_bytes(buf))
+}
+
+/**
+ * Read a "little-endian" i8 from the specified bit source. Since big-endian
+ * and little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `read_i8`, `read_i8_le`, and `read_i8_ne` are provided for the
+ * sake of uniformity.
+ */
+pub fn read_i8_le(src: &mut dyn Read) -> io::Result<i8> {
+    let mut buf = [0; 1];
+    src.read_exact(&mut buf)?;
+    Ok(i8::from_le_bytes(buf))
+}
+
+/**
+ * Read a "network-endian" i8 from the specified bit source. Since big-endian
+ * and little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `read_i8`, `read_i8_le`, and `read_i8_ne` are provided for the
+ * sake of uniformity.
+ */
+pub fn read_i8_ne(src: &mut dyn Read) -> io::Result<i8> {
+    let mut buf = [0; 1];
+    src.read_exact(&mut buf)?;
+    Ok(i8::from_ne_bytes(buf))
 }
 
 /// Read a big-endian u16 from the specified bit source.
@@ -45,6 +97,34 @@ pub fn read_u16_le(src: &mut dyn Read) -> io::Result<u16> {
     Ok(u16::from_le_bytes(buf))
 }
 
+/// Read a network-endian u16 from the specified bit source.
+pub fn read_u16_ne(src: &mut dyn Read) -> io::Result<u16> {
+    let mut buf = [0; 2];
+    src.read_exact(&mut buf)?;
+    Ok(u16::from_ne_bytes(buf))
+}
+
+/// Read a big-endian i16 from the specified bit source.
+pub fn read_i16(src: &mut dyn Read) -> io::Result<i16> {
+    let mut buf = [0; 2];
+    src.read_exact(&mut buf)?;
+    Ok(i16::from_be_bytes(buf))
+}
+
+/// Read a little-endian i16 from the specified bit source.
+pub fn read_i16_le(src: &mut dyn Read) -> io::Result<i16> {
+    let mut buf = [0; 2];
+    src.read_exact(&mut buf)?;
+    Ok(i16::from_le_bytes(buf))
+}
+
+/// Read a network-endian i16 from the specified bit source.
+pub fn read_i16_ne(src: &mut dyn Read) -> io::Result<i16> {
+    let mut buf = [0; 2];
+    src.read_exact(&mut buf)?;
+    Ok(i16::from_ne_bytes(buf))
+}
+
 /// Read a big-endian u32 from the specified bit source.
 pub fn read_u32(src: &mut dyn Read) -> io::Result<u32> {
     let mut buf = [0; 4];
@@ -57,6 +137,34 @@ pub fn read_u32_le(src: &mut dyn Read) -> io::Result<u32> {
     let mut buf = [0; 4];
     src.read_exact(&mut buf)?;
     Ok(u32::from_le_bytes(buf))
+}
+
+/// Read a network-endian u32 from the specified bit source.
+pub fn read_u32_ne(src: &mut dyn Read) -> io::Result<u32> {
+    let mut buf = [0; 4];
+    src.read_exact(&mut buf)?;
+    Ok(u32::from_ne_bytes(buf))
+}
+
+/// Read a big-endian i32 from the specified bit source.
+pub fn read_i32(src: &mut dyn Read) -> io::Result<i32> {
+    let mut buf = [0; 4];
+    src.read_exact(&mut buf)?;
+    Ok(i32::from_be_bytes(buf))
+}
+
+/// Read a little-endian i32 from the specified bit source.
+pub fn read_i32_le(src: &mut dyn Read) -> io::Result<i32> {
+    let mut buf = [0; 4];
+    src.read_exact(&mut buf)?;
+    Ok(i32::from_le_bytes(buf))
+}
+
+/// Read a network-endian i32 from the specified bit source.
+pub fn read_i32_ne(src: &mut dyn Read) -> io::Result<i32> {
+    let mut buf = [0; 4];
+    src.read_exact(&mut buf)?;
+    Ok(i32::from_ne_bytes(buf))
 }
 
 /// Read a big-endian u64 from the specified bit source.
@@ -73,6 +181,34 @@ pub fn read_u64_le(src: &mut dyn Read) -> io::Result<u64> {
     Ok(u64::from_le_bytes(buf))
 }
 
+/// Read a network-endian u64 from the specified bit source.
+pub fn read_u64_ne(src: &mut dyn Read) -> io::Result<u64> {
+    let mut buf = [0; 8];
+    src.read_exact(&mut buf)?;
+    Ok(u64::from_ne_bytes(buf))
+}
+
+/// Read a big-endian i64 from the specified bit source.
+pub fn read_i64(src: &mut dyn Read) -> io::Result<i64> {
+    let mut buf = [0; 8];
+    src.read_exact(&mut buf)?;
+    Ok(i64::from_be_bytes(buf))
+}
+
+/// Read a little-endian i64 from the specified bit source.
+pub fn read_i64_le(src: &mut dyn Read) -> io::Result<i64> {
+    let mut buf = [0; 8];
+    src.read_exact(&mut buf)?;
+    Ok(i64::from_le_bytes(buf))
+}
+
+/// Read a network-endian i64 from the specified bit source.
+pub fn read_i64_ne(src: &mut dyn Read) -> io::Result<i64> {
+    let mut buf = [0; 8];
+    src.read_exact(&mut buf)?;
+    Ok(i64::from_ne_bytes(buf))
+}
+
 /// Read a big-endian u128 from the specified bit source.
 pub fn read_u128(src: &mut dyn Read) -> io::Result<u128> {
     let mut buf = [0; 16];
@@ -87,24 +223,62 @@ pub fn read_u128_le(src: &mut dyn Read) -> io::Result<u128> {
     Ok(u128::from_le_bytes(buf))
 }
 
+/// Read a network-endian u128 from the specified bit source.
+pub fn read_u128_ne(src: &mut dyn Read) -> io::Result<u128> {
+    let mut buf = [0; 16];
+    src.read_exact(&mut buf)?;
+    Ok(u128::from_ne_bytes(buf))
+}
+
+/// Read a big-endian i128 from the specified bit source.
+pub fn read_i128(src: &mut dyn Read) -> io::Result<i128> {
+    let mut buf = [0; 16];
+    src.read_exact(&mut buf)?;
+    Ok(i128::from_be_bytes(buf))
+}
+
+/// Read a little-endian i128 from the specified bit source.
+pub fn read_i128_le(src: &mut dyn Read) -> io::Result<i128> {
+    let mut buf = [0; 16];
+    src.read_exact(&mut buf)?;
+    Ok(i128::from_le_bytes(buf))
+}
+
+/// Read a network-endian i128 from the specified bit source.
+pub fn read_i128_ne(src: &mut dyn Read) -> io::Result<i128> {
+    let mut buf = [0; 16];
+    src.read_exact(&mut buf)?;
+    Ok(i128::from_ne_bytes(buf))
+}
+
 /// Read a big-endian f32 from the specified bit source.
 pub fn read_f32(src: &mut dyn Read) -> io::Result<f32> {
-    read_u32(src).map(f32::from_bits)
+    f32::from_bits(read_u32(src)?)
 }
 
 /// Read a little-endian f32 from the specified bit source.
 pub fn read_f32_le(src: &mut dyn Read) -> io::Result<f32> {
-    read_u32_le(src).map(f32::from_bits)
+    f32::from_bits(read_u32_le(src)?)
+}
+
+/// Read a network-endian f32 from the specified bit source.
+pub fn read_f32_ne(src: &mut dyn Read) -> io::Result<f32> {
+    f32::from_bits(read_u32_ne(src)?)
 }
 
 /// Read a big-endian f64 from the specified bit source.
 pub fn read_f64(src: &mut dyn Read) -> io::Result<f64> {
-    read_u64(src).map(f64::from_bits)
+    f64::from_bits(read_u64(src)?)
 }
 
 /// Read a little-endian f64 from the specified bit source.
 pub fn read_f64_le(src: &mut dyn Read) -> io::Result<f64> {
-    read_u64_le(src).map(f64::from_bits)
+    f64::from_bits(read_u64_le(src)?)
+}
+
+/// Read a network-endian f64 from the specified bit source.
+pub fn read_f64_ne(src: &mut dyn Read) -> io::Result<f64> {
+    f64::from_bits(read_u64_ne(src)?)
 }
 
 /// Read `length` bytes into a new `Vec` from the specified bit source.
@@ -115,7 +289,7 @@ pub fn read_bytes(src: &mut dyn Read, length: u64) -> io::Result<Vec<u8>> {
     if let Err(e) = handle.read_to_end(&mut buf) {
         let msg = format!(
             "Expected {} bytes, but ran into an error instead: {:?}",
-            length, e
+            length, e,
         );
         Err(Error::new(e.kind(), msg))
     } else {
@@ -127,8 +301,8 @@ pub fn read_bytes(src: &mut dyn Read, length: u64) -> io::Result<Vec<u8>> {
  * Write a "big-endian" u8 to the specified bit sink. Since big-endian and
  * little-endian refer to byte order, not bit order, there is no difference
  * between the big-endian and little-endian encodings of a single byte.
- * Nevertheless, both `write_u8` and `write_u8_le` are provided for the sake of
- * uniformity.
+ * Nevertheless, `write_u8`, `write_u8_le`, and `write_u8_ne` are provided for
+ * the sake of uniformity.
  */
 pub fn write_u8(out: &mut dyn Write, val: u8) -> io::Result<()> {
     out.write_all(&val.to_be_bytes())
@@ -138,11 +312,55 @@ pub fn write_u8(out: &mut dyn Write, val: u8) -> io::Result<()> {
  * Write a "little-endian" u8 to the specified bit sink. Since big-endian and
  * little-endian refer to byte order, not bit order, there is no difference
  * between the big-endian and little-endian encodings of a single byte.
- * Nevertheless, both `write_u8` and `write_u8_le` are provided for the sake of
- * uniformity.
+ * Nevertheless, `write_u8`, `write_u8_le`, and `write_u8_ne` are provided for
+ * the sake of uniformity.
  */
 pub fn write_u8_le(out: &mut dyn Write, val: u8) -> io::Result<()> {
     out.write_all(&val.to_le_bytes())
+}
+
+/**
+ * Write a "network-endian" u8 to the specified bit sink. Since big-endian and
+ * little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `write_u8`, `write_u8_le`, and `write_u8_ne` are provided for
+ * the sake of uniformity.
+ */
+pub fn write_u8_ne(out: &mut dyn Write, val: u8) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
+/**
+ * Write a "big-endian" i8 to the specified bit sink. Since big-endian and
+ * little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `write_i8`, `write_i8_le`, and `write_i8_ne` are provided for
+ * the sake of uniformity.
+ */
+pub fn write_i8(out: &mut dyn Write, val: i8) -> io::Result<()> {
+    out.write_all(&val.to_be_bytes())
+}
+
+/**
+ * Write a "little-endian" i8 to the specified bit sink. Since big-endian and
+ * little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `write_i8`, `write_i8_le`, and `write_i8_ne` are provided for
+ * the sake of uniformity.
+ */
+pub fn write_i8_le(out: &mut dyn Write, val: i8) -> io::Result<()> {
+    out.write_all(&val.to_le_bytes())
+}
+
+/**
+ * Write a "network-endian" i8 to the specified bit sink. Since big-endian and
+ * little-endian refer to byte order, not bit order, there is no difference
+ * between the big-endian and little-endian encodings of a single byte.
+ * Nevertheless, `write_i8`, `write_i8_le`, and `write_i8_ne` are provided for
+ * the sake of uniformity.
+ */
+pub fn write_i8_ne(out: &mut dyn Write, val: i8) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
 }
 
 /// Write a big-endian u16 to the specified bit sink.
@@ -155,6 +373,26 @@ pub fn write_u16_le(out: &mut dyn Write, val: u16) -> io::Result<()> {
     out.write_all(&val.to_le_bytes())
 }
 
+/// Write a network-endian u16 to the specified bit sink.
+pub fn write_u16_ne(out: &mut dyn Write, val: u16) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
+/// Write a big-endian i16 to the specified bit sink.
+pub fn write_i16(out: &mut dyn Write, val: i16) -> io::Result<()> {
+    out.write_all(&val.to_be_bytes())
+}
+
+/// Write a little-endian i16 to the specified bit sink.
+pub fn write_i16_le(out: &mut dyn Write, val: i16) -> io::Result<()> {
+    out.write_all(&val.to_le_bytes())
+}
+
+/// Write a network-endian i16 to the specified bit sink.
+pub fn write_i16_ne(out: &mut dyn Write, val: i16) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
 /// Write a big-endian u32 to the specified bit sink.
 pub fn write_u32(out: &mut dyn Write, val: u32) -> io::Result<()> {
     out.write_all(&val.to_be_bytes())
@@ -163,6 +401,26 @@ pub fn write_u32(out: &mut dyn Write, val: u32) -> io::Result<()> {
 /// Write a little-endian u32 to the specified bit sink.
 pub fn write_u32_le(out: &mut dyn Write, val: u32) -> io::Result<()> {
     out.write_all(&val.to_le_bytes())
+}
+
+/// Write a network-endian u32 to the specified bit sink.
+pub fn write_u32_ne(out: &mut dyn Write, val: u32) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
+/// Write a big-endian i32 to the specified bit sink.
+pub fn write_i32(out: &mut dyn Write, val: i32) -> io::Result<()> {
+    out.write_all(&val.to_be_bytes())
+}
+
+/// Write a little-endian i32 to the specified bit sink.
+pub fn write_i32_le(out: &mut dyn Write, val: i32) -> io::Result<()> {
+    out.write_all(&val.to_le_bytes())
+}
+
+/// Write a network-endian i32 to the specified bit sink.
+pub fn write_i32_ne(out: &mut dyn Write, val: i32) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
 }
 
 /// Write a big-endian u64 to the specified bit sink.
@@ -175,6 +433,26 @@ pub fn write_u64_le(out: &mut dyn Write, val: u64) -> io::Result<()> {
     out.write_all(&val.to_le_bytes())
 }
 
+/// Write a network-endian u64 to the specified bit sink.
+pub fn write_u64_ne(out: &mut dyn Write, val: u64) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
+/// Write a big-endian i64 to the specified bit sink.
+pub fn write_i64(out: &mut dyn Write, val: i64) -> io::Result<()> {
+    out.write_all(&val.to_be_bytes())
+}
+
+/// Write a little-endian i64 to the specified bit sink.
+pub fn write_i64_le(out: &mut dyn Write, val: i64) -> io::Result<()> {
+    out.write_all(&val.to_le_bytes())
+}
+
+/// Write a network-endian i64 to the specified bit sink.
+pub fn write_i64_ne(out: &mut dyn Write, val: i64) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
 /// Write a big-endian u128 to the specified bit sink.
 pub fn write_u128(out: &mut dyn Write, val: u128) -> io::Result<()> {
     out.write_all(&val.to_be_bytes())
@@ -183,6 +461,26 @@ pub fn write_u128(out: &mut dyn Write, val: u128) -> io::Result<()> {
 /// Write a little-endian u128 to the specified bit sink.
 pub fn write_u128_le(out: &mut dyn Write, val: u128) -> io::Result<()> {
     out.write_all(&val.to_le_bytes())
+}
+
+/// Write a network-endian u128 to the specified bit sink.
+pub fn write_u128_ne(out: &mut dyn Write, val: u128) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
+}
+
+/// Write a big-endian i128 to the specified bit sink.
+pub fn write_i128(out: &mut dyn Write, val: i128) -> io::Result<()> {
+    out.write_all(&val.to_be_bytes())
+}
+
+/// Write a little-endian i128 to the specified bit sink.
+pub fn write_i128_le(out: &mut dyn Write, val: i128) -> io::Result<()> {
+    out.write_all(&val.to_le_bytes())
+}
+
+/// Write a network-endian i128 to the specified bit sink.
+pub fn write_i128_ne(out: &mut dyn Write, val: i128) -> io::Result<()> {
+    out.write_all(&val.to_ne_bytes())
 }
 
 /// Write a big-endian f32 to the specified bit sink.
@@ -195,6 +493,11 @@ pub fn write_f32_le(out: &mut dyn Write, val: f32) -> io::Result<()> {
     write_u32_le(out, val.to_bits())
 }
 
+/// Write a network-endian f32 to the specified bit sink.
+pub fn write_f32_ne(out: &mut dyn Write, val: f32) -> io::Result<()> {
+    write_u32_ne(out, val.to_bits())
+}
+
 /// Write a big-endian f64 to the specified bit sink.
 pub fn write_f64(out: &mut dyn Write, val: f64) -> io::Result<()> {
     write_u64(out, val.to_bits())
@@ -203,6 +506,11 @@ pub fn write_f64(out: &mut dyn Write, val: f64) -> io::Result<()> {
 /// Write a little-endian f64 to the specified bit sink.
 pub fn write_f64_le(out: &mut dyn Write, val: f64) -> io::Result<()> {
     write_u64_le(out, val.to_bits())
+}
+
+/// Write a network-endian f64 to the specified bit sink.
+pub fn write_f64_ne(out: &mut dyn Write, val: f64) -> io::Result<()> {
+    write_u64_ne(out, val.to_bits())
 }
 
 /// Write the specified `Vec` of bytes to the specified bit sink.
@@ -230,7 +538,7 @@ where
     let _ = src.read_line(&mut buf)?;
     match buf.trim().parse() {
         Ok(x) => Ok(x),
-        Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+        Err(e) => Err(Error::new(ErrorKind::InvalidData, e)),
     }
 }
 
